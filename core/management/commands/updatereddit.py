d4a0736b753bee0ad4a6f825d7406c7699048529
Adam Findlay <skylinerspeeder@gmail.com>
2017-11-15 22:16:38 +0000

    Added alliance flair template addition/deletion

--- core/management/commands/updatereddit.py
+++ core/management/commands/updatereddit.py
@@ -10,4 +10,21 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         sub = get_subreddit()
-        alliances = Alliance.objects.filter(active=True).all()
+
+
+        # Remove inactive alliance flairs and build the alliance flair list
+        live_alliance_flairs = []
+        for flair in sub.flair.templates:
+            alliance = Alliance.objects.filter(name=flair['flair_text']).first()
+            if alliance != None:
+                if alliance.active:
+                    live_alliance_flairs.append(alliance.id)
+                else:
+                    # Remove the alliance
+                    sub.flair.templates.delete(flair['flair_template_id'])
+                    print "Removed %s as it was no longer active" % alliance.name
+
+        # Add newly active alliance flairs
+        for alliance in Alliance.objects.filter(active=True).exclude(id__in=live_alliance_flairs).all():
+            sub.flair.templates.add(alliance.name, css_class="a%i" % alliance.id, text_editable=False)
+            print "Added %s" % alliance.name
