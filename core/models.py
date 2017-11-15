1758a9f5e0c737c5c90aadaf8107db0b9fa5f9c9
Adam Findlay <skylinerspeeder@gmail.com>
2017-11-15 17:56:20 +0000

    Added corp/alliance models

--- core/models.py
+++ core/models.py
@@ -1,6 +1,17 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Corp(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=128)
+    ticker = models.CharField(max_length=5)
+    logo = models.ImageField(upload_to="corps")
+
+
+class Alliance(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=128)
+    ticker = models.CharField(max_length=5)
+    logo = models.ImageField(upload_to="alliances")
