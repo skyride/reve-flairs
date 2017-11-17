0f0d7b35afe8b759ba2c42b585f57b08cd909b03
Adam Findlay <skylinerspeeder@gmail.com>
2017-11-17 00:14:54 +0000

    Added diff set to find krab guilds

--- core/management/commands/setallianceactive.py
+++ core/management/commands/setallianceactive.py
@@ -14,6 +14,8 @@ class Command(BaseCommand):
         # Get the most active pvp alliances
         active_alliances = list(Alliance.objects.filter(generic_logo=False).order_by('-recent_kills').values_list('alliance_id', flat=True)[:250])
 
+        krabs = set(largest_alliances) - set(active_alliances)
+        #print Alliance.objects.filter(alliance_id__in=krabs).all()
         id_set = set(largest_alliances + active_alliances)
         print len(id_set)
 
