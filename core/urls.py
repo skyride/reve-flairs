ce81284e6ea5742bb39856e7555b46d84e473d0e
Adam Findlay <skylinerspeeder@gmail.com>
2017-12-16 03:11:17 +0000

    Added flair views

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,16 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
+from core import views
+
 
 urlpatterns = [
+    url(r'^$', views.all_top100_stats, name="home"),
+    url(r'^alliances/$', views.alliance_stats, name="alliances"),
+    url(r'^corps/$', views.corp_stats, name="corps"),
+    url(r'^generics/$', views.generic_stats, name="generics"),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -2,5 +2,117 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.db.models import Count, Case, When, Sum
+from imagekit import ImageSpec, register
+from imagekit.processors import ResizeToFill
 
-# Create your views here.
+from core.models import *
+
+
+# Annotation filter to find redditorflair objects with
+redditorflair_filter = Sum(
+    Case(
+        When(redditorflairs__ended__isnull=True, then=1),
+        default=0,
+        output_field=models.IntegerField()
+    )
+)
+
+
+class Thumbnail(ImageSpec):
+    processors = [ResizeToFill(38, 38)]
+    format = 'PNG'
+    options = {'quality': 90}
+register.generator('core:logo', Thumbnail)
+
+
+def all_top100_stats(request):
+    alliances = Alliance.objects.filter(
+        active=True
+    ).annotate(
+        flair_count=redditorflair_filter
+    ).order_by(
+        '-flair_count',
+        'name'
+    )
+    corps = Corp.objects.filter(
+        active=True
+    ).annotate(
+        flair_count=redditorflair_filter
+    ).order_by(
+        '-flair_count',
+        'name'
+    )
+    generics = Generic.objects.filter(
+        active=True
+    ).annotate(
+        flair_count=redditorflair_filter
+    ).order_by(
+        '-flair_count',
+        'name'
+    )
+
+    top = list(alliances) + list(corps) + list(generics)
+    top = sorted(top, key=lambda x: x.flair_count, reverse=True)
+
+    context = {
+        "header": "Top 100",
+        "flairs": enumerate(top[:100])
+    }
+
+    return render(request, "core/flair_list.html", context)
+
+
+def alliance_stats(request):
+    alliances = Alliance.objects.filter(
+        active=True
+    ).annotate(
+        flair_count=redditorflair_filter
+    ).order_by(
+        '-flair_count',
+        'name'
+    )
+
+    context = {
+        "header": "Alliances",
+        "flairs": enumerate(alliances)
+    }
+
+    return render(request, "core/flair_list.html", context)
+
+
+def corp_stats(request):
+    corps = Corp.objects.filter(
+        active=True
+    ).annotate(
+        flair_count=redditorflair_filter
+    ).order_by(
+        '-flair_count',
+        'name'
+    )
+
+    context = {
+        "header": "Corps",
+        "flairs": enumerate(corps)
+    }
+
+    return render(request, "core/flair_list.html", context)
+
+
+def generic_stats(request):
+    generics = Generic.objects.filter(
+        active=True
+    ).annotate(
+        flair_count=redditorflair_filter
+    ).order_by(
+        '-flair_count',
+        'name'
+    )
+
+    context = {
+        "header": "Generics",
+        "flairs": enumerate(generics)
+    }
+
+    return render(request, "core/flair_list.html", context)
