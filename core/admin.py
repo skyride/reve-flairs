ffa7a2c09a154b67ac16af030282e9edbf11578b
Adam Findlay <skylinerspeeder@gmail.com>
2020-06-12 19:56:10 +0000

    Added admin

--- core/admin.py
+++ core/admin.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Alliance, Corp, Generic
+
+
+@admin.register(Alliance)
+class AllianceAdmin(admin.ModelAdmin):
+    list_display = (
+        "name", "ticker", "alliance_id", "active", "generic_logo", "characters", "active_chars")
+    list_filter = ("active", "generic_logo")
+
+@admin.register(Corp)
+class CorpAdmin(admin.ModelAdmin):
+    list_display = ("name", "ticker", "corp_id", "member_count")
+    list_filter = ("active", )
+
+@admin.register(Generic)
+class GenericAdmin(admin.ModelAdmin):
+    list_display = ("name", "active")
+    list_filter = ("active", )
